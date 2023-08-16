5823a59eeed05e9c47badaeecf4806de120a630e
Vinicius Santos <viniciusantos0898@gmail.com>
2023-08-16 11:48:09 -0300

    Added styling the Edit Note screen

---
+++ app/src/main/java/com/example/notesapp/presentation/OpenNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notesapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notesapp.R
+
+class OpenNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_note)
+    }
+}
